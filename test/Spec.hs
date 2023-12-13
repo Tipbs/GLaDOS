85a1f582e2fac95feae0d00dd34b321d0289ff51
MizuriGit <tristan.masselot@epitech.eu>
2023-12-13 16:22:26 +0100

    feat: setup HUnit

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+import Test.HUnit
+import qualified System.Exit
+
+test1 :: Test
+test1 = TestCase (assertEqual "should return 3" (3 :: Integer) ((+) 1 2))
+
+tests :: Test
+tests = TestList [TestLabel "test1" test1]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    result <- runTestTT tests
+    if failures result > 0 then System.Exit.exitFailure else System.Exit.exitSuccess
