dec7ab1a36cc4ab13b03de2cfa7031bc75145539
Aurelien Masset <aurelien.masset@epitech.eu>
2023-12-15 15:06:04 +0100

    Added Atom, List and DottedList handling

--- src/Bytecoder.hs
+++ src/Bytecoder.hs
@@ -1,8 +1,5 @@
-module Bytecoder (compileToBytecode) where
-import Data.Maybe (isJust, fromJust, maybeToList)
-import Data.List (unfoldr)
-import Control.Applicative
-import Lib (LispVal(Atom, List, DottedList, Number, String, Bool))
+module Bytecoder (compileToBytecode, showBytesGlobal) where
+import Lib (LispVal(Atom, List, Number, String, Bool, DottedList))
 import Data.Bits
 import Data.Int
 import Data.Char
@@ -14,11 +11,11 @@ pushBool _ = [False, False, False, True]
 
 numToBoolArr :: Integer -> Int -> Int -> [Bool]
 numToBoolArr _ a b | a == b = []
-numToBoolArr num count max = numToBoolArr num (count+1) max ++ [num `testBit` count]
+numToBoolArr num count maxi = numToBoolArr num (count+1) maxi ++ [num `testBit` count]
 
 numToBoolArr2 :: Int -> Int -> Int -> [Bool]
 numToBoolArr2 _ a b | a == b = []
-numToBoolArr2 num count max = numToBoolArr2 num (count+1) max ++ [num `testBit` count]
+numToBoolArr2 num count maxi = numToBoolArr2 num (count+1) maxi ++ [num `testBit` count]
 
 pushNumber :: Integer -> [Bool]
 pushNumber num = [True, False] ++ numToBoolArr num 0 64
@@ -26,17 +23,20 @@ pushNumber num = [True, False] ++ numToBoolArr num 0 64
 getStrLen :: String -> Integer
 getStrLen [] = 0
 getStrLen (_:b) = 1 + getStrLen b
-
 strToBoolArr :: String -> [Bool]
-strToBoolArr str = foldr (\ a -> (++) (numToBoolArr2 (ord a) 0 8)) [] str
+strToBoolArr = foldr (\ a -> (++) (numToBoolArr2 (ord a) 0 8)) []
 
 pushString :: String -> [Bool]
 pushString str = [False, True] ++ numToBoolArr (getStrLen str) 0 12 ++ strToBoolArr str  -- 8 = Max 256 char. 12 = Max 4096 Char. 16 = Max 65536 Char (Unicode may count more than 1)
 
+pushAtom :: String -> [Bool]
+pushAtom a = [True, True] ++ numToBoolArr (getStrLen a) 0 4 ++ strToBoolArr a
+
 compileToBytecode :: LispVal -> [Bool]
--- compileToBytecode (Atom ato) = pushAtom ato
--- compileToBytecode (List exprs) = concatMap compileToBytecode exprs
--- compileToBytecode (DottedList exprs last) = concatMap compileToBytecode (exprs ++ '.' ++ last)
+compileToBytecode (Atom ato) = pushAtom ato
+compileToBytecode (List exprs) = numToBoolArr2 (ord '[') 0 8 ++ concatMap compileToBytecode exprs ++ numToBoolArr2 (ord ']') 0 8
+compileToBytecode (DottedList exprs lasto) = numToBoolArr2 (ord '{') 0 8 ++ concatMap compileToBytecode exprs 
+    ++ numToBoolArr2 (ord '.') 0 8 ++ compileToBytecode lasto ++ numToBoolArr2 (ord '}') 0 8
 compileToBytecode (Number num) = pushNumber num
 compileToBytecode (String str) = pushString str
 compileToBytecode (Bool bol) = pushBool bol
@@ -47,10 +47,11 @@ showBytesGlobal li = "[" ++ showBytes li ++ " ]"
 showBytes :: [Bool] -> String
 showBytes [] = ""
 showBytes (a:b:r) = " " ++ showBit a ++ showBit b ++ showBytes r
+showBytes _ = []
 
 showBit :: Bool -> [Char]
 showBit True = "1"
 showBit False = "0"
 
 test :: LispVal -> String
-test l = showBytesGlobal( compileToBytecode l )
+test l = showBytesGlobal ( compileToBytecode l )
