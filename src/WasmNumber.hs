7fb18d0a579433e01f8f4d0d7ed2ac5709fd0fde
Nestyles <tom.sancho@epitech.eu>
2024-01-12 17:27:51 +0100

    [FIX] buildNumber was setting high byte in reverse

--- src/WasmNumber.hs
+++ src/WasmNumber.hs
@@ -1,4 +1,4 @@
-module WasmNumber (buildNumber, decodeNumber) where
+module WasmNumber (buildNumber, buildWords, decodeNumber) where
 import Data.Binary (Word8, Word32)
 import Data.Bits (Bits(shiftR, shiftL, (.&.), (.|.)))
 
@@ -6,14 +6,15 @@ buildWords :: Int -> [Word8]
 buildWords 0 = []
 buildWords nb = rmLastB : buildWords shifted
     where
-        rmLastB = (.&.) (fromIntegral nb :: Word8) 0x7f
+        rmLastB = (fromIntegral nb :: Word8) .&. 0x7f
         shifted = shiftR nb 7
 
 -- https://en.wikipedia.org/wiki/LEB128
 buildNumber :: Int -> [Word8]
+buildNumber 0 = [0x00]
 buildNumber nb = setHighestByte
     where
-        sepBySeven = buildWords nb
+        sepBySeven = reverse $ buildWords nb
         tailBitSet = map (.|. 128) (tail sepBySeven)
         setHighestByte = head sepBySeven : tailBitSet
 
