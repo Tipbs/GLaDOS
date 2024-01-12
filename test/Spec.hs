118d642a8020c0bbd062e89fba9ef9f1caefcb27
Nestyles <tom.sancho@epitech.eu>
2024-01-12 17:31:19 +0100

    [ADD] Test the compilation of a function call

--- test/Spec.hs
+++ test/Spec.hs
@@ -18,7 +18,7 @@ testCompileOp = TestCase (assertEqual "wrong compileOp output" [0x6a] (compileOp
 
 -- compileExpr (List [Atom "add", Number 5]) [Func "add" ["a", "b"] [Number 5], Func "sub" ["a", "b"] [Number 5]] []
 testFunctionCall :: Test
-testFunctionCall = TestCase (assertEqual "wrong output for good function call" (Right ([], [], [])) (compileExpr (List [Atom "add", Number 5]) [Func "add" ["a", "b"] [Number 5], Func "sub" ["a", "b"] [Number 5]] [] []))
+testFunctionCall = TestCase (assertEqual "wrong output for good function call" (Right ([0x41, 0x5, 0x10, 0x00], [], [])) (compileExpr (List [Atom "add", Number 5]) [Func "add" ["a", "b"] [Number 5], Func "sub" ["a", "b"] [Number 5]] [] []))
 
 testBuildWords :: Test
 testBuildWords = TestCase (assertEqual "buildWords for 624485" [101, 14, 38] (buildWords 624485))
@@ -39,8 +39,8 @@ wasmTests = TestList [
         TestLabel "build op with '+' with CompileOp" testCompileOp,
         TestLabel "build unsigned words for 0x65 to ULEB128" testBuildWords,
         TestLabel "build unsigned number 0x65 to ULEB128" testBuildUnsignedNumber,
-        TestLabel "build unsigned number 624485 to ULEB128" testBuildUnsignedNumber2
-        -- TestLabel "build successful function call" testFunctionCall 
+        TestLabel "build unsigned number 624485 to ULEB128" testBuildUnsignedNumber2,
+        TestLabel "build successful function call" testFunctionCall 
     ]
 
 main :: IO ()
