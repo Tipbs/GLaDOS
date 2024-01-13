8749bed427c8ba97c462ce2cd7650584a0f026bf
TAILLAN Lucas <lucas.taillan@epitech.eu>
2024-01-13 15:58:56 +0100

    [REMOVE] comments that only helped in development

--- src/Wasm.hs
+++ src/Wasm.hs
@@ -31,77 +31,6 @@ magic = [0x00, 0x61, 0x73, 0x6d]
 version :: [Word8]
 version = [0x01, 0x00, 0x00, 0x00]
 
--- 0000000: 0061 736d                                 ; WASM_BINARY_MAGIC
--- 0000004: 0100 0000                                 ; WASM_BINARY_VERSION
--- ; section "Type" (1)
--- 0000008: 01                                        ; section code
--- 0000009: 07                                        ; section size (guess)
--- 000000a: 01                                        ; num types
--- ; func type 0
--- 000000b: 60                                        ; func
--- 000000c: 02                                        ; num params
--- 000000d: 7f                                        ; i32
--- 000000e: 7f                                        ; i32
--- 000000f: 01                                        ; num results
--- 0000010: 7f                                        ; i32
--- ; section "Function" (3)
--- 0000011: 03                                        ; section code
--- 0000012: 02                                        ; section size (guess)
--- 0000013: 01                                        ; num functions
--- 0000014: 00                                        ; function 0 signature index
--- ; section "Code" (10)
--- 0000015: 0a                                        ; section code
--- 0000016: 09                                        ; section size (guess)
--- 0000017: 01                                        ; num functions
--- ; function body 0
--- 0000018: 07                                        ; func body size (guess)
--- 0000019: 00                                        ; local decl count
--- 000001a: 20                                        ; local.get
--- 000001b: 00                                        ; local index
--- 000001c: 20                                        ; local.get
--- 000001d: 01                                        ; local index
--- 000001e: 6a                                        ; i32.add
--- 000001f: 0b                                        ; end
-
--- https://webassembly.github.io/spec/core/binary/modules.html
-
--- fn add(a, b)
--- {
---     var c = a + b;
---     return c;
--- }
-
--- tableau local [a, b, ?]
--- stack []
-
--- Section Code, function body 0:
--- 0d ; body byte len
--- 01 ; local decl count (nombre de declarations de type)
--- 01 ; local type count
--- 7f ; i32
--- 20 ; local.get
--- 00 ; local index
--- 20 ; local.get
--- 01 ; local index ; stack = [a, b]
--- 6a ; i32.add ; stack = [a+b]
--- 21 ; local.set
--- 02 ; local index ; stack = []
--- 20 ; local.get
--- 02 ; stack = [c]
--- 0b ; end
-
--- ; section "Type" (1)
--- 0000008: 01                                        ; section code
--- 0000009: 07                                        ; section size (guess)
--- 000000a: 01                                        ; num types
--- ; func type 0
--- 000000b: 60                                        ; func
--- 000000c: 02                                        ; num params
--- 000000d: 7f                                        ; i32
--- 000000e: 7f                                        ; i32
--- 000000f: 01                                        ; num results
--- 0000010: 7f                                        ; i32
-
 buildSectionHeader :: Word8 -> Int -> Int -> [Word8]
 buildSectionHeader code size nb = [code] ++ buildNumber newSize ++ newNb
     where
@@ -123,13 +52,6 @@ buildSectionType functions = buildSectionHeader 0x01 section_size (length functi
 
 debugHex :: [Word8] -> [String]
 debugHex = map (`showHex` "")
-
--- ; section "Function" (3)
--- 0000011: 03                                        ; section code
--- 0000012: 02                                        ; section size (guess)
--- 0000013: 01                                        ; num functions
--- 0000014: 00                                        ; function 0 signature index
--- should get all functions and assignate them to their index
 
 buildFunctionSec :: [LispVal] -> [Word8]
 buildFunctionSec functions = buildSectionHeader 0x03 section_size (length functions) ++ concated
