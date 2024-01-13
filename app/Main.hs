e2b6038d8c3cbf9b82cdc9083eb737984f0fe9f0
Nestyles <tom.sancho@epitech.eu>
2024-01-13 17:18:02 +0100

    [FEAT] Write the compiled output to file

--- app/Main.hs
+++ app/Main.hs
@@ -7,33 +7,15 @@ import Wasm (buildWasm)
 import Data.Binary (Word8)
 import KopeParser (parseFile)
 import KopeParserLib (KopeVal (KopeArray))
+import qualified Data.ByteString as BS
 
-type Compile = String
+type Compile = (String, String) -- input output
 type Exec = String
+type Output = String
 data Args = Args (Maybe Compile) (Maybe Exec)
 
-readLine :: String -> IO String
-readLine str = putStr str >> hFlush stdout >> getLine
-
-evalReadedLine :: [(String, LispVal)] -> IO ()
-evalReadedLine env = do
-    line <- readLine ">> "
-    case line of
-        "quit" -> return ()
-        _ -> do
-                let (newStr, newEnv) = evalArgs env line
-                putStrLn newStr
-                evalReadedLine newEnv
-
-evalArgs :: Env -> String -> (String, Env)
-evalArgs env arg = newEval evaled
-    where
-        evaled = readExpr arg >>= eval env
-        newEval (Left err) = (show err, env)
-        newEval (Right (newVal, newEnv)) = (show newVal, newEnv)
-
 parseArgs :: [String] -> Maybe Args
-parseArgs ["-c", str] = Just $ Args (Just str) Nothing
+parseArgs ["-c", str, "-o", out] = Just $ Args (Just (str, out)) Nothing
 parseArgs ["-e", str] = Just $ Args Nothing (Just str)
 parseArgs _ = Nothing
 
@@ -45,17 +27,17 @@ buildFile path = do
         (Just (KopeArray arr)) -> return $ buildWasm arr
         _ -> return $ Left "Impossible case"
 
-printBuilded :: String -> IO ()
-printBuilded path = do
-    builded <- buildFile path
+printBuilded :: String -> String -> IO ()
+printBuilded input output = do
+    builded <- buildFile input
     case builded of
-        (Right val) -> print val
+        (Right val) -> BS.writeFile output (BS.pack val)
         (Left err) -> putStrLn err
 
 main :: IO ()
 main = do
     args <- getArgs
     case parseArgs args of
-        Just (Args (Just compile) Nothing) -> printBuilded compile
+        Just (Args (Just (input, output)) Nothing) -> printBuilded input output
         Just (Args Nothing (Just exec)) -> putStrLn ("compiled: " ++ exec)
-        _ -> hPutStrLn stderr "USAGE: ./glados [-c file.kop] | [-e file.wasm]"
+        _ -> hPutStrLn stderr "USAGE: ./glados [-c file.kop -o output.wasm] | [-e file.wasm]"
