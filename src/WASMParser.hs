ca0182587500cca5e3aae4419801870ca07f3ad3
Nestyles <tom.sancho@epitech.eu>
2024-01-14 14:34:44 +0100

    [FEAT] Whole parsing of function headers (names + nbParams)

--- src/WASMParser.hs
+++ src/WASMParser.hs
@@ -6,6 +6,7 @@ import Data.Word
 import WasmNumber (decodeNumber)
 import Data.Bits ((.&.))
 import Control.Monad (replicateM)
+import Data.Char (chr)
 
 data WasmModule = WasmModule {
     wasmFuncs :: [WasmFunction],
@@ -13,7 +14,8 @@ data WasmModule = WasmModule {
 }
 
 data WasmFunction = WasmFunction {
-    nbParams :: Int
+    nbParams :: Int,
+    name :: [Char]
     -- paramsType :: [ParamsType]
 }
 
@@ -56,34 +58,11 @@ parseLebWords = do
 parseNumber :: Get Int
 parseNumber = decodeNumber <$> parseLebWords
 
-parseWasmFunctions :: Bool -> Get WasmFunction
-parseWasmFunctions doParse = do
-    if not doParse
-        then
-            return WasmFunction { nbParams = 0, paramsType = []}
-        else do
-            paramsNb <- parseNumber
-            params <- getWasmType paramsNb []
-            return WasmFunction { nbParams = paramsNb, paramsType = params }
-
 validWasmFunction :: [WasmFunction] -> [WasmFunction]
 validWasmFunction [] = []
 validWasmFunction (x:xs)
     | nbParams x == 0 = validWasmFunction xs
     | otherwise = x : validWasmFunction xs
-
-parseWasmBytes :: WasmModule -> Get WasmModule
-parseWasmBytes wasmModule = do
-    empty <- isEmpty
-    if empty
-        then
-            return WasmModule { wasmFuncs = validWasmFunction (wasmFuncs wasmModule), wasmFuncBodies = []}
-        else do
-            byte <- getWord8
-            funcs <- case byte of
-                0x60 -> parseWasmFunctions True
-                _ -> parseWasmFunctions False
-            parseWasmBytes WasmModule { wasmFuncs = wasmFuncs wasmModule ++ [funcs], wasmFuncBodies = []}
 
 parseDeclCount :: Get Int
 parseDeclCount = do
@@ -145,12 +124,42 @@ parseSectionFunction = do
             replicateM num_funcs parseNumber
         _ -> fail "Section Type was not followed with section function"
 
+parseSectionExport :: Get [(Int, [Char])]
+parseSectionExport = do
+    code <- getWord8
+    case code of
+        0x07 -> do
+            _ <- parseNumber
+            nb_exports <- parseNumber
+            replicateM nb_exports parseString
+        _ -> fail "Export section was not found after section function (mandatory to find main)"
+    where
+        parseString :: Get (Int, [Char])
+        parseString = do
+            len <- parseNumber
+            string <- replicateM len getWord8
+            export_kind <- getWord8
+            index <- parseNumber
+            case export_kind of
+                0x00 -> return (index, map (chr . fromIntegral) string)
+                0x02 -> return (-1, map (chr . fromIntegral) string)
+                _ -> fail "Unknown export type"
+
+
 parseSections :: Get WasmModule
 parseSections = do
     type_sec <- parseSectionType
     func_sec <- parseSectionFunction
-    let functions_header = map (\i -> type_sec !! i) func_sec
-    
+    exports <- parseSectionExport
+    let functions_params = map (type_sec !!) func_sec
+    let functions_strings = mapM (`lookup` exports) [0..length functions_params]
+    case functions_strings of
+        Just names -> do
+            let functions = map (\(n, p_count) -> WasmFunction {nbParams = p_count, name = n}) (zip names functions_params)
+            return WasmModule {wasmFuncs = functions}
+        _ -> fail "One function had no export which match its index"
+    -- let functions_parsed = map (\i p_count -> WasmFunction {nbParams = p_count, name = lookup exports !! i}) (zip [0..length functions_params] functions_params)
+
 
 parseWasmModule :: Get WasmModule
 parseWasmModule = do
